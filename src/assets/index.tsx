48a1d3abfc528cc6580bb4a776433016f61ba0b2
pinkjagnie <olga.krzysztofik@gmail.com>
2023-03-11 17:53:14 +0100

    Added photos to assets

---
+++ src/assets/index.tsx
@@ -0,0 +1,17 @@
+import polarBear from "./polarBear.png";
+import cheetah from "./cheetah.png";
+import panda from "./panda.png";
+import fox from "./fox.png";
+import squirrel from "./squirrel.png";
+import butterfly from "./butterfly.png";
+import elephant from "./elephant.png";
+
+export {
+  polarBear,
+  cheetah,
+  panda,
+  fox,
+  squirrel,
+  butterfly,
+  elephant
+}
